0d59ebaa223cddd42b35a6d2ce2a2b37cf3e1a05
bikalfm <bikal@finalmoment.xyz>
2024-09-10 16:45:00 -0700

    Update page.tsx

--- simple-ai-chatbot-main/app/page.tsx
+++ simple-ai-chatbot-main/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <main className="flex flex-col h-screen bg-[#040d17] text-white">
       <nav className="flex justify-between items-center p-4">
       </nav>
-      <center> <h3> FM.Ai </h3> </center>
+      <center> <h3> CliniQ.AI </h3> </center>
       <div className="flex-grow overflow-hidden">
         <Chat />
       </div>
